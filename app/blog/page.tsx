ecb79c847de92c3fd37ddf64d2a4712a8c7fde83
kurokawa96-96 <22wwnk65ny@privaterelay.appleid.com>
2026-07-22 17:29:51 +0900

    page.tsx を更新

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,1 +1,12 @@
+import ComingSoon from "../../components/ui/ComingSoon";
+
+export default function BlogPage() {
+  return (
+    <ComingSoon
+      label="BLOG"
+      title="ブログ"
+      description="制作の過程や理論の裏側について、少しずつ書いていく予定です。"
+    />
+  );
+}
 
